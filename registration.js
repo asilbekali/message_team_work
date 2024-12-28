fdf58b1dd5591e224cbcf4dec5a6b4d1fcbf9928
asilbekali <asilbekabdugafforov7@gmail.com>
2024-12-29 00:17:50 +0500

    koda jonatganda api ga saqlamayapti lekin shablon tayor

--- registration.js
+++ registration.js
@@ -1,11 +1,21 @@
-let Uname = document.querySelector(".fullname-input")
-let Euser = document.querySelector(".email-input")
+async function just() {
+    let username = document.getElementById("fullname-input");
+    let userEmail = document.getElementById("email-input"); 
+    let passUser = document.getElementById("password-input");
+    let data = {
+        name: username.value,
+        username: userEmail.value,
+        password: passUser.value,
+    };
 
-function setData(api){
-    axios.get(api).then(
-        (setD) =>{
-            let setDa = setD.data
-            console.log("Set")
-        }
-    )
+    const apiURL = "https://6764fcfe52b2a7619f5e0263.mockapi.io/userData";
+
+    try {
+        const response = await axios.post(apiURL, data);
+        alert("Ma'lumot muvaffaqiyatli saqlandi:", response.data);
+        alert("Ma'lumot muvaffaqiyatli saqlandi!");
+    } catch (error) {
+        console.error("Xatolik yuz berdi:", error);
+        alert("Ma'lumotni saqlashda xatolik yuz berdi!");
+    }
 }
