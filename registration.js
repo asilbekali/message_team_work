0015f88a56a47cce8e6a22056631e699fa0df259
abdulbori <abdulborimahamatjanov86@gmail.com>
2024-12-30 19:46:13 +0600

    initial commit

--- registration.js
+++ registration.js
@@ -4,42 +4,28 @@ const passwordInput = document.getElementById("password-input");
 const signUpbtn = document.getElementById("signUp");
 const passwordEye = document.getElementById("password-eyes");
 
-passwordEye.addEventListener("click", (e) => {
-  console.log(e.target);
-});
-
 signUpbtn.addEventListener("click", (e) => {
+  let registration = JSON.parse(localStorage.getItem("registration")) || [];
+
   if (
-    fullNameInput.value === "" ||
-    emalInput.value === "" ||
-    passwordInput.value === ""
+    fullNameInput.value !== "" &&
+    emalInput.value !== "" &&
+    passwordInput.value !== ""
   ) {
-    alert("Iltimos barcha ma'lumotlaringizni kiriting!");
-  } else {
-    alert("Muvaffaqiyatli ro'yxatdan o'ttingiz!");
+    let registerItem = {
+      fullNameInput: fullNameInput.value,
+      emalInput: emalInput.value,
+      passwordInput: passwordInput.value,
+    };
+
+    registration.push(registerItem);
+    localStorage.setItem("registration", JSON.stringify(registration));
+
+    alert("Ro'yxatdan muvaffaqiyatli o'ttingiz!");
     fullNameInput.value = "";
     emalInput.value = "";
     passwordInput.value = "";
+  } else {
+    alert("Iltimos barcha ma'lumotlarni kiriting!");
   }
 });
-// async function just() {
-//   let username = document.getElementById("fullname-input");
-//   let userEmail = document.getElementById("email-input");
-//   let passUser = document.getElementById("password-input");
-//   let data = {
-//     name: username.value,
-//     username: userEmail.value,
-//     password: passUser.value,
-//   };
-
-//   const apiURL = "https://6764fcfe52b2a7619f5e0263.mockapi.io/userData";
-
-//   try {
-//     const response = await axios.post(apiURL, data);
-//     alert("Ma'lumot muvaffaqiyatli saqlandi:", response.data);
-//     alert("Ma'lumot muvaffaqiyatli saqlandi!");
-//   } catch (error) {
-//     console.error("Xatolik yuz berdi:", error);
-//     alert("Ma'lumotni saqlashda xatolik yuz berdi!");
-//   }
-// }
